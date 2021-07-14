f88baf3b0172311c4e63e383f422e8e627e4c1cb
harrymcg <harry.mcguire2@gmail.com>
2021-07-14 11:13:03 +1000

    Working sidenav with photos.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutMeComponent } from './about-me/about-me.component';
+//import { AppComponent } from './app.component';
 
 
 const routes: Routes = [
   {path: 'about-me', component: AboutMeComponent}
+  //{path: 'app', component: AppComponent}
 ];
 
 @NgModule({
