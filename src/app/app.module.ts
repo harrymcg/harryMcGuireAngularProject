0cf590d4e9cabd9d5941bc1c5531030a11688d92
harrymcg <harry.mcguire2@gmail.com>
2021-07-14 10:08:19 +1000

    Working responsive sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+
 import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
@@ -15,8 +21,8 @@ import { AboutMeComponent } from './about-me/about-me.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    MatSliderModule
+    //material design imports:
+    BrowserAnimationsModule, MatSliderModule, MatSidenavModule, MatToolbarModule, MatButtonModule, MatIconModule, MatDividerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
