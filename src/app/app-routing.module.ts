d0c6332d42497d68b95d767ef6cfacc236e35a6a
harrymcg <harry.mcguire2@gmail.com>
2021-07-16 11:54:52 +1000

    Fixed home page routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,17 +6,18 @@ import { HowComponent } from './how/how.component';
 import { WhatsNextComponent } from './whats-next/whats-next.component';
 //import { AppComponent } from './app.component';
 
-
 const routes: Routes = [
-  {path: 'about-me', component: AboutMeComponent},
-  {path: 'whats-next', component: WhatsNextComponent},
-  {path: 'home', component:HomeComponent},
-  {path: 'how', component: HowComponent}
-  //{path: 'app', component: AppComponent}
+  { path: 'about-me', component: AboutMeComponent },
+  { path: 'whats-next', component: WhatsNextComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'how', component: HowComponent },
+
+  //** = wildcard, and if nothing matches, it will default to this. therefore, this MUST go at the bottom, or else it'll override every other routing element and ruin it */
+  { path: '**', component: HomeComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
